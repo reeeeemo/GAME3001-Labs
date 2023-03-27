76dc1793ed4c75df656675a376a18a498378aea0
reeeeemo <robbie@oxley.ca>
2023-03-26 23:57:26 -0400

    [ADD] Obstacle

--- GAME3001-W2023-Assignment03/src/Obstacle.cpp
+++ GAME3001-W2023-Assignment03/src/Obstacle.cpp
@@ -6,7 +6,7 @@
 
 Obstacle::Obstacle()
 {
-	TextureManager::Instance().Load("../Assets/sprites/Player/rock.png", "obstacle");
+	TextureManager::Instance().Load("../Assets/textures/rock.png", "obstacle");
 
 	const auto size = TextureManager::Instance().GetTextureSize("obstacle");
 	SetWidth(static_cast<int>(size.x));
