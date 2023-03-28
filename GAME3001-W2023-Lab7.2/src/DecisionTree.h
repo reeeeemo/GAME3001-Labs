dfde6a152481bce7302d3e3c6b6999aae83dc51f
reeeeemo <robbie@oxley.ca>
2023-03-28 11:20:13 -0400

    [ADD] Lab 7.2

---
+++ GAME3001-W2023-Lab7.2/src/DecisionTree.h
@@ -0,0 +1,59 @@
+#pragma once
+#ifndef __DECISION_TREE_H__
+#define __DECISION_TREE_H__
+
+#include "Agent.h"
+#include "CloseCombatCondition.h"
+#include "LOSCondition.h"
+#include "RadiusCondition.h"
+#include "TreeNode.h"
+#include "TreeNodeType.h"
+
+#include <vector>
+
+class DecisionTree
+{
+public:
+	// Constructor(s)
+	DecisionTree();
+	DecisionTree(Agent* agent);
+
+	// Destructor(s)
+	virtual ~DecisionTree();
+
+	// Accessors and Mutators
+	Agent* GetAgent() const;
+	void SetAgent(Agent* agent);
+
+	// Condition Node Accessors
+	[[nodiscard]] LOSCondition* GetLOSNode() const;
+	[[nodiscard]] RadiusCondition* GetRadiusNode() const;
+	[[nodiscard]] CloseCombatCondition* GetCloseCombatNode() const;
+
+	// Public utility methods / convenience methods
+	TreeNode* AddNode(TreeNode* parent, TreeNode* child_node, TreeNodeType type);
+
+	// Life Cycle Methods
+	void Display() const;
+	void Update();
+	void Clean();
+
+	// Main public method
+	void MakeDecision() const; // In-order traversal
+
+private:
+	// private data members (fields)
+	Agent* m_agent{};
+	LOSCondition* m_LOSNode{};
+	RadiusCondition* m_radiusNode{};
+	CloseCombatCondition* m_closeCombatNode{};
+
+	// Tree node list
+	std::vector<TreeNode*> m_treeNodeList;
+
+	// Private methods
+	void m_buildTree();
+
+};
+
+#endif //!__DECISION_TREE_H__
